3ef5b7ddbb57d4cb21b8ce209e87ea4a199ffa1c
clmtf <clement.tikhomiroff@gmail.com>
2026-03-30 18:18:56 +0200

    implementation users

--- src/classes/Utilisateurs.java
+++ src/classes/Utilisateurs.java
@@ -4,4 +4,40 @@ abstract public class Utilisateurs {
     int id;
     String login;
     String pw;//mdp
+
+    public Utilisateurs(){
+        // constructeur vide
+    }
+    public Utilisateurs(int id,String login,String pw){
+        this.id = id;
+        this.login = login;
+        this.pw = pw;
+    }
+    abstract public void seConnecter();
+    abstract public void consulterCatalogue();
+    //*Getter et Setter *//
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
 }
