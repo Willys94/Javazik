39a2007194ff4aad9dd323290f75011736cfe0c4
Willys Martias <martiaswillys@gmail.com>
2026-04-04 23:44:20 +0200

    Class Morceau et Utilisateurs

--- src/classes/Utilisateurs.java
+++ src/classes/Utilisateurs.java
@@ -1,7 +1,18 @@
 package classes;
 
 abstract public class Utilisateurs {
-    int id;
-    String login;
-    String pw;//mdp
+    protected int id;
+    protected String login;
+    protected String pw;//mdp
+
+    Utilisateurs(int id, String login, String pw) {
+        this.id = id;
+        this.login = login;
+        this.pw = pw;
+    }
+
+    public boolean Login(String login, String pw) {
+
+        return login.equals(this.login) && pw.equals(this.pw); // retourne 1 si les logs sont correct 0 sinon
+    }
 }
